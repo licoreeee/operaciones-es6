9d27a9138946b0561816bdea6a4eba90a06041bf
Victoria Vega <victoria.vega247804@potros.itson.edu.mx>
2025-09-16 13:20:42 -0700

    Suma y resta
    Agregué suma y resta y documenté

---
+++ operaciones.js
@@ -0,0 +1,35 @@
+/* Ricardo Alán Gutiérrez Garcés - 247398
+   Abel Eduardo Sánchez Guerrero - 245473
+   Diego Valenzuela Parra - 247700
+   Victoria Vega Bernal - 247804 */
+
+// Exportamos la función suma.
+export function suma(a, b) {
+    return a + b;
+}
+
+//Exportamos la función resta.
+export function resta(a, b) {
+    return a - b;
+}
+
+//Exportamos la función multiplicación.
+export function multiplicacion(a,b){
+    return a * b;
+}
+
+//Creamos la función división.
+function division(a, b) {
+    return a / b;
+}
+
+// Creamos la constante pi.
+const pi = 3.1416;
+
+// Exportamos las dos entidades.
+export {division, pi}
+
+// Exportamos la función.
+export function modulo(a, b) {
+    return a % b;
+}
